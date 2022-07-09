012c8051fa1e038b2cd5868921e39c7dbede3cab
strikingLoo <lucianostrika44@gmail.com>
2022-07-09 03:46:30 -0300

    display confidence

--- suggest_crystalization.py
+++ suggest_crystalization.py
@@ -22,6 +22,9 @@ start = time.time()
 key = args[0]
 best_match = find_best_match(pd.read_csv('protein_base.csv'), key, int(args[1]), .1)
 end = time.time()
+similarity = list(best_match['fuzzy_similarity'])[0]
+
+print( f"Match found! Confidence (similarity score): {similarity}")
 
 for k, v in dict(best_match).items():
     if (k == 'pdb_id') or (list(v)[0] == 1):
